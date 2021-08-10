dbbe45c68efd74a2220d8c7699977e60251618c7
Vodkarm06 <79284263+Vodkarm@users.noreply.github.com>
2021-08-10 03:26:15 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,5 +2,5 @@
 title VNitro Installer
 cls
 pip install -r requirements.txt
-echo Installation finished
+echo Installation finished, you can now use VNitro ! Check Github page for learn how to use, Enjoy with it ! :)
 pause
